8b3150de3aa69eca34f67c5079eadcf93172d8b1
Martin Beroiz <martinberoiz@gmail.com>
2018-10-29 15:39:45 -0500

    Added bare bones scheduler.py file

---
+++ scheduler.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+Scheduler Module
+
+This is just a file to write first attempts at a scheduler module.
+
+"""
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == '__main__':
+    main()
