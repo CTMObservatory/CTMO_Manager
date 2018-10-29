d2560bda5412af96345495172fec7f64a42afc97
Martin Beroiz <martinberoiz@gmail.com>
2018-10-29 17:20:53 -0500

    Simple work order python dictionary sent over XMLRPC

--- scheduler.py
+++ scheduler.py
@@ -5,12 +5,32 @@ Scheduler Module
 
 This is just a file to write first attempts at a scheduler module.
 
+(c) TOROS Project
 """
 
 
-def main():
-    print("Hello World!")
+import xmlrpc.client
+TELESCOPE_IP = "http://localhost:8000/"
+
+
+def send_work_order():
+    work_order = {
+        "Priority": 1,
+        "Coordinates": {"RA": 23.1, "Dec": 13.2},
+        "Filter": "I",
+        "Author": "Main Module"
+    }
+
+    with xmlrpc.client.ServerProxy(TELESCOPE_IP) as telescope_module:
+        try:
+            telescope_module.front_desk(work_order)
+        except xmlrpc.client.ProtocolError as err:
+            print("A protocol error occurred")
+            print("URL: %s" % err.url)
+            print("HTTP/HTTPS headers: %s" % err.headers)
+            print("Error code: %d" % err.errcode)
+            print("Error message: %s" % err.errmsg)
 
 
 if __name__ == '__main__':
-    main()
+    send_work_order()
