4f3740503a2ec619f722eb3617e1cd7919cf1de6
Martin Beroiz <martinberoiz@gmail.com>
2018-10-29 15:46:14 -0500

    More boilerplate skeleton files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='scheduler',
+      version='0.0',
+      description='Job Manager for CTMO System',
+      author='TOROS Dev Team',
+      author_email='ctmo@utrgv.edu',
+      url='https://toros.utrgv.edu',
+      py_modules=['scheduler', ],
+      install_requires=[],
+      test_suite='tests',
+      )
---
+++ tests/test_scheduler.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestScheduler(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_01(self):
+        print("Write the test!")
+
+
+if __name__ == "__main__":
+    unittest.main()
