e405e16f7496ec00e8c53a52bed7a259a18796a1
Martin Beroiz <martinberoiz@gmail.com>
2019-03-09 21:03:04 -0600

    Fixed some bugs. Tested in VM. Bumped version.

--- ctmomanager/__init__.py
+++ ctmomanager/__init__.py
@@ -1,7 +1,7 @@
 from . import telescope
 from . import scheduler
 
-__version__ = '0.1a9'
+__version__ = '0.1a10'
 
 # Set up logging
 from loguru import logger
@@ -10,7 +10,6 @@ logger.remove()
 from . import config
 log_config = config.get_config_for_key('Logging') or {}
 log_file = log_config.get('File')
-log_file = None
 if log_file is not None:
     log_level = log_config.get('Log Level') or 'INFO'
     logger.add(log_file,
