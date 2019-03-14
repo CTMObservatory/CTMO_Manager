98c519fad0c5f0569ecb2774c07cf93e10bf7e00
Martin Beroiz <martinberoiz@gmail.com>
2019-03-14 13:29:55 -0500

    Added telescope files

---
+++ telescope.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Scheduler Module
+
+(c) GAIA-CTMO
+"""
+import datetime
+import xmlrpc.client
+import yaml
+
+__version__ = '0.1a1'
+
+import yaml
+CONFIG_PATH = '/etc/ctmo/ctmo.conf.yaml'
+
+config = None
+with open(CONFIG_PATH) as f:
+    config = yaml.load(f.read())
+net_address = config.get('Telescope Address')
+
+def front_desk(work_order):
+    for k, v in work_order.items():
+        print("{}:\t{}".format(k, v))
+    return "Work order received."
+
+def serve():
+    from xmlrpc.server import SimpleXMLRPCServer
+
+    server = SimpleXMLRPCServer((net_address.get('IP'), net_address.get('Port')))
+    server.register_function(front_desk, "front_desk")
+    server.serve_forever()
+
+if __name__ == '__main__':
+    serve()
